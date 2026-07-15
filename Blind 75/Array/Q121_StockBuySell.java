dffb6706400ad9be2c0c898cd6a0ee3d6cb7f8e2
Suhan Ganesh <suhanganesh.05@gmail.com>
2026-07-15 21:19:36 +0530

    Stock Buy and Sell

--- Blind 75/Array/Q121_StockBuySell.java
+++ Blind 75/Array/Q121_StockBuySell.java
@@ -1,3 +1,8 @@
+/* 
+There is a brute approach to this by using two loops. 
+Optimal solution is to set minProfit to the highest integer value and set maxProfit to 0. then update the minPrice only if the current price is less than min, else calculate and compare the max value.
+*/
+
 class Solution {
     public static int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
